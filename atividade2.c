235790eb499ff145d5ee8ba0781b9e60c1d3796f
sunHelen12 <sunrise.developer12@gmail.com>
2025-02-03 22:00:50 -0300

    Alteração dos LEDs

--- atividade2.c
+++ atividade2.c
@@ -12,29 +12,28 @@
 
 //variável de controle
 bool sequence_active = false; //indica se a sequência está ativa
-int64_t turn_off_green_callback(alarm_id_t id, void *user_data);
+int64_t turn_off_blue_callback(alarm_id_t id, void *user_data);
 
-
-//rotina de callback para desligar o LED verde
-int64_t turn_off_green_callback(alarm_id_t id, void *user_data) {
-    gpio_put(LED_PIN_GREEN, false); //desliga o LED verde
-    sequence_active = false; //permite nova ativação pelo botão
-    return 0;
-}
-
-//rotina de callback para desligar o LED vermelho e ligar o LED verde
-int64_t turn_off_red_callback(alarm_id_t id, void *user_data) { 
-    gpio_put(LED_PIN_RED, false); //desliga o LED vermelho
-    gpio_put(LED_PIN_GREEN, true); //desliga o LED verde
-    add_alarm_in_ms(3000, turn_off_green_callback, NULL, false); //agenda o desligamento do LED verde
-    return 0;
-}
-
-//rotina de callback para desligar o LED azul e ligar o LED vermelho 
+//rotina de callback para desligar o LED azul
 int64_t turn_off_blue_callback(alarm_id_t id, void *user_data) { 
-    gpio_put(LED_PIN_BLUE, false); //desliga o LED azul
-    gpio_put(LED_PIN_RED, true); //liga o LED vermelho
-    add_alarm_in_ms(3000, turn_off_red_callback, NULL, false); //agenda o desligamento do LED vermelho
+    gpio_put(LED_PIN_BLUE, false); // Desliga o LED azul
+    sequence_active = false; // Permite nova ativação pelo botão
+    return 0;
+}
+
+//rotina de callback para desligar o LED vermelho e ligar o LED azul
+int64_t turn_off_red_callback(alarm_id_t id, void *user_data) { 
+    gpio_put(LED_PIN_RED, false); // Desliga o LED vermelho
+    gpio_put(LED_PIN_BLUE, true); // Liga o LED azul
+    add_alarm_in_ms(3000, turn_off_blue_callback, NULL, false); // Agenda o desligamento do LED azul
+    return 0;
+}
+
+//rotina de callback para desligar o LED verde e ligar o LED vermelho
+int64_t turn_off_green_callback(alarm_id_t id, void *user_data) {
+    gpio_put(LED_PIN_GREEN, false); // Desliga o LED verde
+    gpio_put(LED_PIN_RED, true); // Liga o LED vermelho
+    add_alarm_in_ms(3000, turn_off_red_callback, NULL, false); // Agenda o desligamento do LED vermelho
     return 0;
 }
 
@@ -75,7 +74,7 @@ int main() {
                 gpio_put(LED_PIN_GREEN, true);
 
                 //inicia a sequência de desligamento com temporizador
-                add_alarm_in_ms(3000, turn_off_blue_callback, NULL, false);
+                add_alarm_in_ms(3000, turn_off_green_callback, NULL, false);
             }
         }
         sleep_ms(10); //pequena pausa para reduzir consumo de CPU
